8a6b176150c52cb4be25fa52a95a91b1327c3fe6
letthem <suyeon3662@naver.com>
2025-07-08 13:20:41 +0900

    fix: 내용 변경

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>멋사 13th Vercel 배포 테스트</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
