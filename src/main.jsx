706fa47d6081f9556ce5de8661822afeb5d45fdc
이윤서 <yyoo04@naver.com>
2025-07-10 20:05:25 +0900

    fix: 오류수정

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
